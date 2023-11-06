cf905b1ce8c8d3add905961a2f5d23c37ca29c22
Kevin Irias <kiriasru@gmail.com>
2023-11-05 19:07:50 -0600

    Se ha cambiado la etiqueta semantica de form a section ya que refrescaba la lista de tareas

--- src/components/TaskForm.jsx
+++ src/components/TaskForm.jsx
@@ -1,19 +1,36 @@
 import {useState} from 'react'
 
 const TaskForm = () => {
+// Configurar useState
+    const [task, setTask] = useState(''); // task debe ir vacio porque no hay tarea agregada aun
+    const [taskList, setTaskList] = useState([]); // lista de tareas
 
-    const [task, setTask] = useState('');
-    
+    const addTask = () => {
+        if (task) {
+            setTaskList([...taskList, task]); // Agrega tasks a la lista
+            setTask(''); // Borra el input despues de que se haya agregado una task
+        }
+    };
+
     return (
-        <form>
-            <input
-                type="text"
-                placeholder="Add your new todo"
-                value={task}
-                onChange = {(e) => setTask(e.target.value)} 
-            />
-            <button>+</button>
-        </form>
+        <div>
+
+            <section>
+                <input
+                    type="text"
+                    placeholder="Add your new todo"
+                    value={task}
+                    onChange = {(e) => setTask(e.target.value)} 
+                />
+                <button onClick={addTask}>+</button>
+            </section>
+
+            <ul>
+                {taskList.map((t, index) => (
+                    <li key={index}>{t}</li>
+                ))}
+            </ul>
+        </div>
     );
 }
 
