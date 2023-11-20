62a9fa1171859d80127aac060fbbd2f19331c659
Kevin Irias <kiriasru@gmail.com>
2023-11-19 20:39:37 -0600

    Se ha implementado el form y se ha implementado la validacion

--- src/components/TaskForm.jsx
+++ src/components/TaskForm.jsx
@@ -4,7 +4,12 @@ import useTaskManager from '../hooks/useTaskManager';
 import { getStoredTasks, storeTasks } from '../utils/localStorage';
 
 const TaskForm = () => {
-  const [task, setTask] = useState({ text: '', description: '', completed: false, isEditing: false });
+  const [task, setTask] = useState({
+    text: '',
+    description: '',
+    completed: false,
+    isEditing: false,
+  });
   const { tasks, addTask, deleteTask, updateTask, setTasks } = useTaskManager([]);
 
   useEffect(() => {
@@ -16,8 +21,20 @@ const TaskForm = () => {
     storeTasks(tasks);
   }, [tasks]);
 
+  const [taskNameError, setTaskNameError] = useState('');
+  const [taskDescError, setTaskDescError] = useState('');
+
   const handleAddTask = () => {
-    if (task.text) {
+    const isTitleEmpty = !task.text.trim();
+    const isDescriptionFilled = !!task.description.trim();
+
+    if (isTitleEmpty && isDescriptionFilled) {
+      setTaskDescError('Task must have a title.');
+      setTaskNameError('');
+    } else if (task.text.length < 3) {
+      setTaskNameError('Task name should be at least 3 characters long.');
+      setTaskDescError('');
+    } else {
       if (task.isEditing) {
         updateTask({ ...task });
         setTask({ text: '', description: '', completed: false, isEditing: false });
@@ -25,6 +42,8 @@ const TaskForm = () => {
         addTask(task);
         setTask({ text: '', description: '', completed: false, isEditing: false });
       }
+      setTaskNameError('');
+      setTaskDescError('');
     }
   };
 
@@ -46,22 +65,46 @@ const TaskForm = () => {
     updateTask(updatedTask);
   };
 
+  const handleTaskChange = (e) => {
+    const { name, value } = e.target;
+    setTask((prevTask) => ({
+      ...prevTask,
+      [name]: value,
+    }));
+    if (name === 'description' && !value.trim() && !task.text.trim()) {
+      setTaskDescError('Task must have a title.');
+    } else {
+      setTaskDescError('');
+    }
+  };
+
+  const handleTaskSubmit = (e) => {
+    e.preventDefault();
+    if (task.text.length >= 3) {
+      handleAddTask();
+    } else {
+      setTaskNameError('Task name should be at least 3 characters long.');
+    }
+  };
+
   return (
     <div>
-      <section>
+      <form onSubmit={handleTaskSubmit}>
         <input
           type="text"
           placeholder="Add your new todo"
+          name="text"
           value={task.text}
-          onChange={(e) => setTask({ ...task, text: e.target.value })}
+          onChange={handleTaskChange}
         />
         <input
           type="text"
           placeholder="Task description"
+          name="description"
           value={task.description}
-          onChange={(e) => setTask({ ...task, description: e.target.value })}
+          onChange={handleTaskChange}
         />
-        <button onClick={handleAddTask}>
+        <button type="submit">
           {task.isEditing ? 'Update' : 'Add'}
         </button>
         {task.isEditing && (
@@ -69,7 +112,19 @@ const TaskForm = () => {
             Cancel
           </button>
         )}
-      </section>
+      </form>
+
+      {taskNameError && (
+        <div className="error" style={{ color: 'red' }}>
+          {taskNameError}
+        </div>
+      )}
+
+      {taskDescError && (
+        <div className="error" style={{ color: 'red' }}>
+          {taskDescError}
+        </div>
+      )}
 
       <ul>
         {tasks.map((t, index) => (
