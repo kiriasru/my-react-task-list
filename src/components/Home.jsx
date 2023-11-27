df1607160c065513632c7f8a9ee09ae609b5999a
Kevin Irias <kiriasru@gmail.com>
2023-11-26 20:59:28 -0600

    Se ha agregado la informacion correspondiente a Home y SobreNosotros

---
+++ src/components/Home.jsx
@@ -0,0 +1,9 @@
+const Home = () => {
+  return (
+    <div>
+      <h2>Bienvenido a la Todo List App</h2>
+    </div>
+  );
+};
+
+export default Home;
---
+++ src/components/SobreNosotros.jsx
@@ -0,0 +1,27 @@
+const SobreNosotros = () => {
+  return (
+    <div>
+        
+      <h2>Acerca de la Aplicación</h2>
+      <h3>Funcionalidad de la Aplicación:</h3>
+      <ul>
+        <li>Agregar tareas con título y descripción</li>
+        <li>Modificar el título y descripción de una tarea</li>
+        <li>Tachar tareas que fueron completadas</li>
+        <li>Eliminar tareas</li>
+      </ul>
+
+      <h3>Tecnologías usadas para su desarrollo:</h3>
+      <ul>
+        <li>React</li>
+        <li>React Router</li>
+        <li>React Icons</li>
+        <li>CSS</li>
+      </ul>
+      
+      
+    </div>
+  );
+};
+
+export default SobreNosotros;
