a2806c318c6dd81cbdcd364784cfec306f61e907
Kevin Irias <kiriasru@gmail.com>
2023-12-19 11:34:48 -0600

    Se ha agregado un modo claro y oscuro usando chakra ui

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,32 @@
+import {
+  ChakraProvider,
+  Flex,
+  IconButton,
+  useColorMode,
+  Heading,
+} from '@chakra-ui/react';
+import { SunIcon, MoonIcon } from '@chakra-ui/icons';
+
 const Header = () => {
+  const { colorMode, toggleColorMode } = useColorMode();
+
   return (
     <header className="header">
-        <h1>Todo App</h1>
+      <ChakraProvider>
+        <Flex alignItems="center" justifyContent="space-between">
+          <Heading as="h1" size="lg">
+            Todo App
+          </Heading>
+          <IconButton
+            aria-label="Toggle color mode"
+            icon={colorMode === 'light' ? <MoonIcon /> : <SunIcon />}
+            onClick={toggleColorMode}
+            variant="ghost"
+          />
+        </Flex>
+      </ChakraProvider>
     </header>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
